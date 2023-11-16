3d8412ed8d5f8ef861c4dd3a89a48a26db32bb1c
Edward Beazer <eddiebeazer@gmail.com>
2023-11-16 17:49:48 -0500

    test

--- src/index.js
+++ src/index.js
@@ -33,7 +33,6 @@ module.exports = async function run() {
     }
                 
     const owner = contextPullRequest.base.repo.owner.login;
-    //const owner = "web"
     const repo = contextPullRequest.base.repo.name;
 
     // The pull request info on the context isn't up to date. When
